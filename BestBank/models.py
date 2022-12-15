4757d2121abec92d5b4874eb7658ee683838bd04
Abdul Moiz Mohammed <63392096+abdulmoiz-m@users.noreply.github.com>
2022-12-15 01:35:59 -0500

    created overview page

--- BestBank/models.py
+++ BestBank/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+# class Transactions(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
--- BestBank/views.py
+++ BestBank/views.py
@@ -1,6 +1,13 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
 # Create your views here.
 def homepage(request):
     return render(request, 'homepage.html')
+
+
+# @login_required(login_url='login')
+# balance and transactions
+def overview(request):
+    return render(request, 'overview.html')
