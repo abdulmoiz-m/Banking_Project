64bdea869b912488ac4e11d7655de8ef5740cafe
StutiRay <135stuti@gmail.com>
2022-12-15 01:18:40 -0500

    Created a new application for accounts

--- BestBank/urls.py
+++ BestBank/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.homepage, name="homepage"),
-    path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
+    path('accounts/', include('accounts.urls')),
+
 ]
